ad5597a16d95d620be0e98a2a13b117ee4a7b77f
ceejayszn <mancollomc@gmail.com>
2026-07-19 14:27:27 +0300

    Fix Render publish directory for React build

--- mobawi_admin/render-build.sh
+++ mobawi_admin/render-build.sh
@@ -4,3 +4,9 @@ set -o errexit
 
 npm install
 npm run build
+
+# Render is looking for build/web (from the previous Flutter setup)
+# so we move our React 'dist' folder contents into 'build/web'
+mkdir -p build/web
+cp -r dist/* build/web/
+
